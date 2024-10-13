7dffdf48249c54be705b10e0d1205c99a8963f6f
bbishal12 <bishalbudhathokee1@gmail.com>
2024-10-13 11:33:56 -0500

    removed print stmt

--- main.java
+++ main.java
@@ -1,8 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World! how are you");
-        System.out.println("Hello, World! masseter");
-        System.out.println("Hello, World! ");
     }
 
 }
